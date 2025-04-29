0e9f751aa9b9eb2b9978c0f5e0dcc049ba81cd93
tanvishetty014 <tanvishetty407@gmail.com>
2025-04-29 16:18:37 +0530

    Second commit

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int num1 = 40;
+        int num2 = 60;
+        int result = num1- num2;
+        System.out.println("Subtraction is: "+result);
+
+    }
+}
