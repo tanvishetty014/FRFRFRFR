cd20e2998142e36bae96ebf9df1ff762ac29739b
tanvishetty014 <tanvishetty407@gmail.com>
2025-04-29 16:22:59 +0530

    third commit

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int num1 = 40;
+        int num2 = 60;
+        int result = num1* num2;
+        System.out.println("Multiplication is: "+result);
+
+    }
+}
