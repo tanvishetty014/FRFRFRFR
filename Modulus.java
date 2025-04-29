a5bb1ad61f1f64336fc6be998f452523fc685989
tanvishetty014 <tanvishetty407@gmail.com>
2025-04-29 16:58:27 +0530

    fifth commit

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+public class Modulus {
+    
+    public static void main(String[] args) {
+        int num1 = 40;
+        int num2 = 60;
+        int result = num1%num2;
+        System.out.println("Modulus is: "+result);
+
+    }
+}
